64a833ccab5bb6bdc4791be2ae2d8ade8a19b277
PKUer_ztw <625581835@qq.com>
2018-10-01 17:35:58 +0800

    firstcommit

---
+++ app/src/main/java/com/example/ztw/myweather/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.ztw.myweather;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+
+/**
+ * Created by ztw on 2018/9/28.
+ */
+public class MainActivity extends ActionBarActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.weather_info);
+    }
+}
